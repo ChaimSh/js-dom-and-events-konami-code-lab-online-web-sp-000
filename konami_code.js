ae202190b1e98b051bf2625633f69c2933f6b8ca
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-12 19:33:15 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const main = document.body
 
+  main.addEventListener('keyDown', function(event) {
+    console.log('i was keyed')
+  }) 
 }
